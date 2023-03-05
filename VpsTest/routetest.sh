1cad608c4c4ff7a16920e90fd4c0bdf61834646e
honorcnboy <68975045+honorcnboy@users.noreply.github.com>
2023-03-06 03:08:22 +0800

    Update routetest.sh

--- VpsTest/routetest.sh
+++ VpsTest/routetest.sh
@@ -103,7 +103,7 @@ for i in {0..14}; do
 echo 
 done > >(tee -a $log_file)
 
-echo "脚本运行时间：$(date +%s)秒"
+echo "脚本运行时间：$(date +%s)秒" | tee -a $log_file
 }
 echo -e "-------------------------------------------------------\n本脚本测试结果为TCP回程路由, 仅供参考.\n" | tee -a $log_file
 echo "检测结果已保存至 ${log_file}，请自行查看"
