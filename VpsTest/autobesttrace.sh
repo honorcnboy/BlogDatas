db92b81e7f3ca41f2cbbf8d357369e31f275ce3a
honorcnboy <68975045+honorcnboy@users.noreply.github.com>
2023-03-05 11:37:28 +0800

    Update autobesttrace.sh

--- VpsTest/autobesttrace.sh
+++ VpsTest/autobesttrace.sh
@@ -46,6 +46,8 @@ ip_addr=(北京电信 北京移动 北京联通 上海电信 上海移动 上海
 now=$(date +"%y%m%d%H%M")
 log_file="/root/besttrace${now}.txt"
 
+for i in {0..14}
+
 echo "
 #======================================
 # Project: AutoBesttrace
@@ -56,7 +58,6 @@ echo "
 #======================================
 "
 
-for i in {0..14}
 do
     echo ${ip_addr[$i]}
     ./besttrace2023 -q 1 -g cn ${ip_list[$i]}
