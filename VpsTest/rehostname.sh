211a9227b905a9f3c635e51c25389b8ae616296e
honorcnboy <68975045+honorcnboy@users.noreply.github.com>
2023-03-13 21:20:51 +0800

    Create rehostname.sh

---
+++ VpsTest/rehostname.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# 预先获取 sudo 权限
+sudo -v
+
+while true; do
+    echo "请输入新的主机名："
+    read new_hostname
+
+    # 检查用户是否有输入主机名
+    if [ -z "$new_hostname" ]; then
+        echo "没有输入主机名"
+    else
+        # 修改主机名
+        echo "$new_hostname" > /etc/hostname
+        hostnamectl set-hostname $new_hostname
+
+        echo "已将主机名修改为 $new_hostname"
+        break
+    fi
+done
+
+# 查询主机名，并将其赋值给变量 "Hostname"
+Hostname=$(hostname)
+
+# 检查 "/etc/hosts" 文件中是否存在 "127.0.0.1 ${Hostname}" 这一行内容
+if grep -q "127.0.0.1 ${Hostname}" /etc/hosts; then
+    echo "已有：127.0.0.1 ${Hostname}"
+else
+    echo "127.0.0.1 ${Hostname}" | sudo tee -a /etc/hosts > /dev/null
+    echo "已添加：127.0.0.1 ${Hostname}"
+fi
+
+# 关闭 while 循环
+done
