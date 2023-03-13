c4a3d92e4ae180b65d5728254b33d8d3d25f301e
honorcnboy <68975045+honorcnboy@users.noreply.github.com>
2023-03-13 21:18:09 +0800

    Update fixhostname.sh

--- FixHostname/fixhostname.sh
+++ FixHostname/fixhostname.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-# 查询主机名"Hostname"
-while true; do
-    echo "请输入新的主机名："
-    read new_hostname
-
-    # 检查用户是否有输入主机名
-    if [ -z "$new_hostname" ]; then
-        echo "没有输入主机名"
-    else
-        # 修改主机名
-        echo "$new_hostname" > /etc/hostname
-        hostnamectl set-hostname $new_hostname
-
-        echo "已将主机名修改为 $new_hostname"
-        break
-    fi
-
 # 查询主机名，并将其赋值给变量 "Hostname"
 Hostname=$(hostname)
 
