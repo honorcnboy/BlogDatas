d99160a61946f586abf53412d8638a544aa2b87f
honorcnboy <68975045+honorcnboy@users.noreply.github.com>
2023-07-25 15:32:59 +0800

    Update firewall_setup.sh

--- VpsScript/firewall_setup.sh
+++ VpsScript/firewall_setup.sh
@@ -28,7 +28,7 @@ prompt_integer() {
     fi
 }
 
-# 常见端口数组
+# 常见端口数组，包括SSH端口22
 common_ports=(22 80 443 53 123)
 
 # Step 1: Check if a firewall is installed
@@ -42,11 +42,11 @@ elif command -v firewalld &>/dev/null; then
 fi
 
 if [ -n "$firewall_installed" ]; then
-    echo "防火墙 ($firewall_installed) 已安装。"
+    echo "已安装防火墙 ($firewall_installed)。"
     prompt_yn "是否继续使用当前防火墙？" && {
-        echo "保留当前防火墙 ($firewall_installed)。"
+        echo "保持当前防火墙 ($firewall_installed)。"
     } || {
-        echo "移除当前防火墙 ($firewall_installed)..."
+        echo "正在移除当前防火墙 ($firewall_installed)..."
         case $firewall_installed in
             "iptables")
                 iptables -F
@@ -69,56 +69,33 @@ fi
 
 # Step 2: Choose a firewall to install
 if [ -z "$firewall_installed" ] || [ $? -eq 1 ]; then
-    echo "选择要安装的防火墙:"
+    echo "选择要安装的防火墙："
     echo "1. iptables"
     echo "2. ufw"
     echo "3. firewalld"
-    firewall_to_install=""
-    while [ -z "$firewall_to_install" ]; do
-        selected_option=$(prompt_integer "请输入相应的数字 (1/2/3): ")
-        case $selected_option in
-            1)
-                firewall_to_install="iptables"
-                ;;
-            2)
-                firewall_to_install="ufw"
-                ;;
-            3)
-                firewall_to_install="firewalld"
-                ;;
-            *)
-                echo "无效的选择，请重新输入。"
-                ;;
-        esac
-    done
+    prompt_integer "请输入相应的数字 (1/2/3)：" 
+    case $? in
+        1)
+            firewall_to_install="iptables"
+            ;;
+        2)
+            firewall_to_install="ufw"
+            ;;
+        3)
+            firewall_to_install="firewalld"
+            ;;
+        *)
+            echo "无效的选择。正在退出。"
+            exit 1
+            ;;
+    esac
 
-    echo "安装 $firewall_to_install..."
+    echo "正在安装 $firewall_to_install..."
     apt-get update
     apt-get install -y $firewall_to_install
 fi
 
-# 添加常见端口的规则
-case $firewall_to_install in
-    "iptables")
-        for port in "${common_ports[@]}"; do
-            iptables -A INPUT -p tcp --dport $port -j ACCEPT
-            iptables -A INPUT -p udp --dport $port -j ACCEPT
-        done
-        ;;
-    "ufw")
-        for port in "${common_ports[@]}"; do
-            ufw allow $port
-        done
-        ;;
-    "firewalld")
-        for port in "${common_ports[@]}"; do
-            firewall-cmd --add-port=$port/tcp
-            firewall-cmd --add-port=$port/udp
-        done
-        ;;
-esac
-
-# Step 3: Start the selected firewall
+# Step 3: Start the selected firewall (without enabling)
 case $firewall_to_install in
     "iptables")
         iptables -P INPUT DROP
@@ -126,37 +103,38 @@ case $firewall_to_install in
         iptables -P OUTPUT ACCEPT
         ;;
     "ufw")
-        ufw default deny incoming
-        ufw default allow outgoing
-        ufw enable
+        # Disable ufw prompts and enable the firewall
+        export UFW_FORCE_PROMPT=YES
+        ufw --force enable
+        unset UFW_FORCE_PROMPT
         ;;
     "firewalld")
         systemctl start firewalld
-        systemctl enable firewalld
+        systemctl disable firewalld
         ;;
 esac
 
 # Step 4: Check the current firewall rules
 case $firewall_to_install in
     "iptables")
-        echo "当前防火墙规则:"
+        echo "当前防火墙规则："
         iptables -L --line-numbers
         ;;
     "ufw")
-        echo "当前防火墙规则:"
+        echo "当前防火墙规则："
         ufw status numbered
         ;;
     "firewalld")
-        echo "当前防火墙规则:"
+        echo "当前防火墙规则："
         firewall-cmd --list-all
         ;;
 esac
 
 # Step 5: Remove specific allowed ports
-prompt_yn "是否要移除特定允许的端口?" && {
-    read -p "请输入要移除的端口号 (如果有多个，请用逗号分隔): " ports_to_remove
-    echo "您输入的要移除的端口号为: $ports_to_remove"
-    prompt_yn "确认移除这些端口号?" && {
+prompt_yn "是否要移除特定允许的端口？" && {
+    read -p "请输入要移除的端口号（多个端口以逗号分隔）：" ports_to_remove
+    echo "您输入的要移除的端口：$ports_to_remove"
+    prompt_yn "确认移除端口吗？" && {
         case $firewall_to_install in
             "iptables")
                 for port in $(echo $ports_to_remove | tr ',' ' '); do
@@ -177,58 +155,67 @@ prompt_yn "是否要移除特定允许的端口?" && {
                 ;;
         esac
     } || {
-        prompt_yn "是否要重新输入要移除的端口号?" && {
-            prompt_yn "确认移除这些端口号?" && {
-                # Repeat confirmation and removal process
+        prompt_yn "是否要重新输入要移除的端口？" && {
+            prompt_yn "确认移除端口吗？" && {
+                # 重复确认和移除过程
             }
         }
     }
 }
 
-# Step 6: Add specific allowed ports
-prompt_yn "是否要添加特定允许的端口?" && {
-    read -p "请输入要添加的端口号 (如果有多个，请用逗号分隔): " ports_to_add
-    echo "您输入的要添加的端口号为: $ports_to_add"
-    prompt_yn "确认添加这些端口号?" && {
-        case $firewall_to_install in
-            "iptables")
-                for port in $(echo $ports_to_add | tr ',' ' '); do
-                    iptables -A INPUT -p tcp --dport $port -j ACCEPT
-                    iptables -A INPUT -p udp --dport $port -j ACCEPT
-                done
-                ;;
-            "ufw")
-                for port in $(echo $ports_to_add | tr ',' ' '); do
-                    ufw allow $port
-                done
-                ;;
-            "firewalld")
-                for port in $(echo $ports_to_add | tr ',' ' '); do
-                    firewall-cmd --add-port=$port/tcp
-                    firewall-cmd --add-port=$port/udp
-                done
-                ;;
-        esac
-    } || {
-        prompt_yn "是否要重新输入要添加的端口号?" && {
-            prompt_yn "确认添加这些端口号?" && {
-                # Repeat confirmation and addition process
-            }
-        }
-    }
-}
-
-# Final Step: Show all currently opened ports
-echo "当前所有已开放的端口："
+# Step 6: Add specific allowed ports (including common ports)
+# 先添加常见端口的规则
 case $firewall_to_install in
     "iptables")
-        iptables -L INPUT -n | grep ACCEPT
+        for port in "${common_ports[@]}"; do
+            iptables -A INPUT -p tcp --dport $port -j ACCEPT
+            iptables -A INPUT -p udp --dport $port -j ACCEPT
+        done
         ;;
     "ufw")
-        ufw status numbered | grep ALLOW
+        for port in "${common_ports[@]}"; do
+            ufw allow $port
+        done
         ;;
     "firewalld")
-        firewall-cmd --list-ports
+        for port in "${common_ports[@]}"; do
+            firewall-cmd --add-port=$port/tcp
+            firewall-cmd --add-port=$port/udp
+        done
+        ;;
+esac
+
+# Step 7: Check the updated firewall rules after adding common ports
+case $firewall_to_install in
+    "iptables")
+        echo "更新后的防火墙规则（包括常见端口）："
+        iptables -L --line-numbers
+        ;;
+    "ufw")
+        echo "更新后的防火墙规则（包括常见端口）："
+        ufw status numbered
+        ;;
+    "firewalld")
+        echo "更新后的防火墙规则（包括常见端口）："
+        firewall-cmd --list-all
+        ;;
+esac
+
+# Step 8: Remove specific allowed ports (if needed)
+# ...（省略部分代码，请根据之前的回答填充）
+
+# Step 9: Add specific allowed ports (if needed)
+# ...（省略部分代码，请根据之前的回答填充）
+
+# Step 10: Enable the firewall (after adding all rules)
+case $firewall_to_install in
+    "iptables")
+        ;;
+    "ufw")
+        ufw --force enable
+        ;;
+    "firewalld")
+        systemctl enable firewalld
         ;;
 esac
 
