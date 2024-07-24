f205cb0950769d211d406c26a96779237721a2fb
honorcnboy <68975045+honorcnboy@users.noreply.github.com>
2024-07-25 00:05:36 +0800

    Update and rename update_xanmod_sysctl.sh to Optimization-v2.sh

--- VpsScript/Optimization-v2.sh
+++ VpsScript/Optimization-v2.sh
@@ -1,10 +1,38 @@
+
 #!/bin/bash
 
-# 备份现有的 /etc/sysctl.conf 文件
+# 更新系统
+apt update && apt upgrade -y
+
+# 检测是否安装iptables，如果没有则安装
+if ! command -v iptables &> /dev/null; then
+echo "安装 iptables..."
+apt install -y iptables
+fi
+
+# 获取所有网络接口的名称
+interfaces=$(nmcli device status | awk '{print $1}' | grep -v DEVICE)
+
+# 循环遍历每个网络接口
+for interface in $interfaces; do
+# 使用nmcli增加环缓冲的大小
+echo "Setting ring buffer size for interface $interface..."
+sudo nmcli connection modify $interface txqueuelen 10000
+
+# 调优网络设备积压队列以避免数据包丢弃
+echo "Tuning network device backlog for interface $interface..."
+sudo nmcli connection modify $interface rxqueuelen 10000
+
+# 增加NIC的传输队列长度
+echo "Increasing NIC transmission queue length for interface $interface..."
+sudo nmcli connection modify $interface transmit-hash-policy layer2+3
+done
+
+# 备份原始配置文件
 cp /etc/sysctl.conf /etc/sysctl.conf.bak
 
-# 替换 /etc/sysctl.conf 文件内容
-cat <<EOF > /etc/sysctl.conf
+# 配置内核参数
+cat << EOF > /etc/sysctl.conf
 # ------ 网络调优: 基本 ------
 # TTL 配置, Linux 默认 64
 # net.ipv4.ip_default_ttl=64
@@ -182,7 +210,56 @@ kernel.sysrq=1
 vm.zone_reclaim_mode=0
 EOF
 
-# 使配置立即生效
+# 应用新的内核参数
 sysctl -p
 
 echo "sysctl 配置已更新并生效。"
+
+# 更新 grub
+update-grub
+
+# 调整网络队列处理算法（Qdiscs），优化TCP重传次数
+for interface in $interfaces; do
+echo "Tuning network queue disciplines (Qdiscs) and TCP retransmission for interface $interface..."
+sudo tc qdisc add dev $interface root fq
+sudo tc qdisc change dev $interface root fq maxrate 90mbit
+sudo tc qdisc change dev $interface root fq burst 15k
+sudo tc qdisc add dev $interface ingress
+sudo tc filter add dev $interface parent ffff: protocol ip u32 match u32 0 0 action connmark action mirred egress redirect dev ifb0
+sudo tc qdisc add dev ifb0 root sfq perturb 10
+sudo ip link set dev ifb0 up
+sudo ethtool -K $interface tx off rx off
+done
+
+# 调整TCP和UDP流量的优先级
+for interface in $interfaces; do
+echo "Setting priority for TCP and UDP traffic on interface $interface..."
+sudo iptables -A OUTPUT -t mangle -p tcp -o $interface -j MARK --set-mark 10
+sudo iptables -A OUTPUT -t mangle -p udp -o $interface -j MARK --set-mark 20
+sudo iptables -A PREROUTING -t mangle -i $interface -j MARK --set-mark 10
+sudo iptables -A PREROUTING -t mangle -p udp -i $interface -j MARK --set-mark 20
+done
+
+# 设置文件描述符限制脚本
+#!/bin/bash
+
+# 获取内存大小（单位：MB）
+total_memory=$(free -m | awk '/^Mem:/{print $2}')
+
+# 计算文件描述符限制数值
+if [[ $total_memory -eq 512 ]]; then
+limit=4096
+else
+# 每增加512MB内存，文件描述符限制数值乘以2
+multiplier=$((total_memory / 512))
+limit=$((4096 * multiplier))
+fi
+
+# 设置文件描述符限制
+echo "* hard nofile $limit" >> /etc/security/limits.conf
+echo "* soft nofile $limit" >> /etc/security/limits.conf
+
+echo "文件描述符限制已设置为 $limit"
+
+# 提示用户重启系统
+echo "系统优化完成,重启系统以应用新的内核"
